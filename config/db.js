25a46158d7144a0db76dc21d09d1dd74c12282a8
ngocmai <ngmai0527@gmail.com>
2026-04-06 23:11:51 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config(); // Đảm bảo đã load file .env
 
 export const connectDB = async () => {
-  const dbUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/BT1_CK";
-
   try {
-    await mongoose.connect(dbUri);
-    console.log("MongoDB connected successfully");
+    const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/BT1_CK";
+    
+    await mongoose.connect(url);
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    console.log("Lỗi kết nối DB:", error.message);
     process.exit(1);
   }
 };
